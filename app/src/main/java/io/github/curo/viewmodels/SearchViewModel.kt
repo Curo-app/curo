1faf4dcf262dbbb6530b9788d6a7f12b6c2cdcfe
RuslanZaripov <zaripovruslan864@gmail.com>
2023-05-22 17:41:54 +0300

    Fix state updating on swipe

--- app/src/main/java/io/github/curo/viewmodels/SearchViewModel.kt
+++ app/src/main/java/io/github/curo/viewmodels/SearchViewModel.kt
@@ -6,13 +6,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import io.github.curo.data.NotePreview
 import io.github.curo.database.dao.NoteDao
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 
 @Stable
@@ -21,11 +19,11 @@ class SearchViewModel(
 ) : FeedViewModel(noteDao) {
     val query: MutableStateFlow<String> = MutableStateFlow("")
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override val feedUiState: StateFlow<FeedUiState> =
         query
-            .mapLatest { noteDao.searchNotes(it).first() }
-            .map { notes -> notes.map { NotePreview.of(it) } }
+            .combine(noteDao.searchNotes(query.value)) { _, searchResult ->
+                searchResult.map { NotePreview.of(it) }
+            }
             .map { notes -> FeedUiState(notes) }
             .stateIn(
                 viewModelScope,
