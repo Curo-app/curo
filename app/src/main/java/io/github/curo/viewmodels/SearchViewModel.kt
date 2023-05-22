271c3eab317b37ef37e627d2e507a606e1951417
RuslanZaripov <zaripovruslan864@gmail.com>
2023-05-22 18:03:17 +0300

    Combine query and notes updating

--- app/src/main/java/io/github/curo/viewmodels/SearchViewModel.kt
+++ app/src/main/java/io/github/curo/viewmodels/SearchViewModel.kt
@@ -6,10 +6,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import io.github.curo.data.NotePreview
 import io.github.curo.database.dao.NoteDao
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
@@ -19,11 +20,13 @@ class SearchViewModel(
 ) : FeedViewModel(noteDao) {
     val query: MutableStateFlow<String> = MutableStateFlow("")
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override val feedUiState: StateFlow<FeedUiState> =
         query
-            .combine(noteDao.searchNotes(query.value)) { _, searchResult ->
-                searchResult.map { NotePreview.of(it) }
+            .flatMapLatest { newQuery ->
+                noteDao.searchNotes(newQuery)
             }
+            .map { searchResult -> searchResult.map { NotePreview.of(it) } }
             .map { notes -> FeedUiState(notes) }
             .stateIn(
                 viewModelScope,
